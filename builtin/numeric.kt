0d631685e7cb8212a92ba62863ab6d2072e31d5a
Jacob Lister <jacob.h.lister@gmail.com>
2025-11-23 22:05:33 +1300

    move test directory

--- builtin/numeric.kt
+++ builtin/numeric.kt
@@ -49,7 +49,7 @@ class intbv : BitvecSignal {
 class i8(val value: Int) : BitvecSignal {
     override val length: Int get() = 8
     override val rawbits: ULong get() = value.toULong()
-    override fun frombits(length: Int, bits: ULong): BitvecSignal = this
+    override fun frombits(length: Int, bits: ULong): BitvecSignal = i8(bits.toInt())
 
     operator fun invoke(next: i8) = this.next(next)
     operator fun plus(other: i8): i8 = i8(this.value + other.value)
---
+++ test/number.kt
@@ -0,0 +1,52 @@
+fun testSignal() {
+    val test_a = i8(2)
+    val test_b = 3.i8
+
+    test_b(123.i8)
+    signalNext()
+
+    println("$test_a + $test_b = ${test_a + test_b}")
+}
+
+fun testBitVecSignal() {
+//    val test_a = intbv(8, 128 + 16 + 13)
+//    val test_b = intbv(8, 3)
+    val test_a = i8(128 + 16 + 13)
+    val test_b = i8(3)
+
+    println("$test_a + $test_b = ${test_a + test_b}")
+
+    val nibble = test_a[7 downTo 4]
+    println(nibble)
+    println(nibble.length)
+    println(nibble.bits)
+    println(test_a.bits)
+
+//    test[7 downTo 0] = a
+//    nibble(test_a[3 downTo 0])
+    nibble(15.i8)
+
+    signalNext()
+    println(nibble)
+    println(nibble.length)
+    println(nibble.bits)
+    println(test_a.bits)
+//    val byte = intbv(nibble, nibble)
+//    println(byte)
+//    println(byte.bits)
+}
+
+
+fun main() {
+//    testSignal()
+    testBitVecSignal()
+
+//    val bitvecInt = Bitvec_Int(0xFFF, 13)
+//    val bitvecI8 = I8(0xFF)
+//
+//    println(bitvecI8.match("11111111"))
+//    println(bitvecI8.x)
+//
+//    logBitvec(bitvecInt)
+//    logBitvec(bitvecI8)
+}
